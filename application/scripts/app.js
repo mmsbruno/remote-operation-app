3eb07733d4675dd142da7d9692d40ef48baf9404
mmsbruno <105087422+mmsbruno@users.noreply.github.com>
2023-02-14 10:18:00 +0000

    added

---
+++ application/scripts/app.js
@@ -0,0 +1,72 @@
+var app = new Vue({
+    el: '#app1',
+    data: {
+        pane: 'connect',
+        address: 'ws://192.168.1.100:9090',
+        path: 'http://192.168.1.100:8080/',
+        username: '',
+        password: '',
+        loginerr: '',
+        users: [
+        ]
+    },
+    methods: {
+        /** Deprecated. */
+        output_test() {
+            init();
+        },
+        /** Validate user and establish connection to ROS. */
+        connect() {
+            var vm = this;
+            if (vm.username == '') {
+                vm.loginerr = 'Please enter username!';
+                return;
+            }
+            if (vm.password == '') {
+                vm.loginerr = 'Please enter password!';
+                return;
+            }
+            if (vm.username != 'admin') {  /** Para alterar o usuário */
+                vm.loginerr = 'User does not exist!';
+                vm.username = '';
+                vm.password = '';
+                return;
+            }
+            if (vm.password != 'admin') { /** Para alterar a senha */
+                vm.loginerr = 'Password incorrect!';
+                vm.password = '';
+                return;
+            }
+            vm.pane = 'default';
+            setTimeout(init(vm.address), 1000);
+        },
+        /** Save the waypoints to external file. */
+        save_points() {
+            var vm = this;
+            var blob = new Blob([JSON.stringify(waypoints)], {type: "application/json;charset=utf-8"});
+            saveAs(blob, "waypoints.json");
+        },
+        /** Trigger the load-file dialog of load input that is hidden. */
+        load_trigger() {
+            $('input#file-points').click();
+        },
+        /** Load the waypoints from external file. */
+        load_points() {
+            var files = $('input#file-points')[0].files;
+            if (files.length <= 0) return false;
+            var fr = new FileReader();
+            fr.onload = function(e) { 
+                waypoints = JSON.parse(e.target.result);
+                createWaypointButtons();
+            }
+            fr.readAsText(files.item(0));
+        }
+    },
+    /** Define the default hostname. */
+    created() {
+        this.address = location.hostname + ':5500';
+        $.getJSON('/data/auth.json', (data) => {
+            this.users = data;
+        });
+    }
+});
